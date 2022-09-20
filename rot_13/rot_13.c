d7e2768a7ad8fa9cda42e1354d268c46f18efc1b
Juan victor Robles trujillo <jrobles-@c1r12s6.42madrid.com>
2022-09-20 13:06:49 +0200

    Final

---
+++ rot_13/rot_13.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void rot_13(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] = str[i] + 13;
+		else if((str[i] >= 'n' && str[i] <='z') || (str[i] >= 'N' && str[i] <= 'Z'))
+			str[i] = str[i] - 13;
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if(argc == 2)
+		rot_13(argv[1]);
+	ft_putchar('\n');
+	return (0);
+}
