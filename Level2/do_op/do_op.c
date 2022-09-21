732c2bed1f6f81c03ceb0607395990f8052ecb9b
Juan victor Robles trujillo <jrobles-@c1r17s2.42madrid.com>
2022-09-21 22:57:38 +0200

    Final

---
+++ Level2/do_op/do_op.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		if (argv[2][0] == '+')
+			printf("%d", (atoi(argv[1]) + atoi(argv[3])));
+		if (argv[2][0] == '-')
+			printf("%d", (atoi(argv[1]) - atoi(argv[3])));
+		if (argv[2][0] == '*')
+			printf("%d", (atoi(argv[1]) * atoi(argv[3])));
+		if (argv[2][0] == '/')
+			printf("%d", (atoi(argv[1]) / atoi(argv[3])));
+		if (argv[2][0] == '%')
+			printf("%d", (atoi(argv[1]) % atoi(argv[3])));
+	}
+	printf("\n");
+	return (0);	
+}
+
---
+++ Level2/ft_atoi/espacios.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+void print()
+{
+	char *c;
+}
