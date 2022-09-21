fdc631c886ec83c217381a7e44db2c32d6cefb72
Juan victor Robles trujillo <jrobles-@c1r17s2.42madrid.com>
2022-09-21 23:10:47 +0200

    Final

--- Level2/ft_atoi/espacios.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-
-void print()
-{
-	char *c;
-}
--- Level2/ft_atoi/ft_atoi.c
+++ Level2/ft_atoi/ft_atoi.c
@@ -1,9 +1,22 @@
-#include <unistd.h>
-
 int	ft_atoi(const char *str)
 {
 	int i;
+	int sig;
+	int result;
 
 	i = 0;
-	if (str[i] 
+	sig = 1;
+	result = 0;
+	if (str[i] >= 9 && str[i] <= 13)
+	   i++;
+	if(str[i] == '-')
+		sig = -1;
+	if(str[i] == '-' || str[i] == '+')
+		i++;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sig * result);		
 }
